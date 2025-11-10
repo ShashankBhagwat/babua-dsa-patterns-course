188f486c5bd07c3ce2da28c2dea96f0b1ca49f42
Shashank Bhagwat <shashankkumar676@gmail.com>
2025-11-10 22:19:03 +0530

    https://leetcode.com/problems/valid-palindrome/

---
+++ Shashank/src/main/java/P001_TwoPointer/P001_ValidPalindrome.java
@@ -0,0 +1,41 @@
+package src.main.java.P001_TwoPointer;
+
+// https://leetcode.com/problems/valid-palindrome/
+public class P001_ValidPalindrome{
+
+    public static void main(String[]args){
+        Solution solution = new Solution();
+        System.out.println(solution.isPalindrome("A man, a plan, a canal: Panama"));
+    }
+
+    static class Solution {
+        public boolean isPalindrome(String s) {
+            int lowerIndex = 0, upperIndex = s.length() - 1;
+
+            while (lowerIndex < upperIndex) {
+
+                if (!Character.isLetterOrDigit(s.charAt(lowerIndex))) {
+                    lowerIndex++;
+                    continue;
+                }
+
+                if (!Character.isLetterOrDigit(s.charAt(upperIndex))) {
+                    upperIndex--;
+                    continue;
+                }
+
+                if (Character.toLowerCase(s.charAt(lowerIndex)) != Character.toLowerCase(s.charAt(upperIndex))) {
+                    return false;
+                }
+
+                lowerIndex++;
+                upperIndex--;
+            }
+
+            return true;
+        }
+    }
+
+
+}
+
